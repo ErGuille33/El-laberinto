8b346a85c93606f30adcc0e3a7925bbefc155551
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2020-12-16 12:36:56 +0100

    Grid de casillas

---
+++ El laberinto y mas/Assets/Scripts/Casilla.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Casilla : MonoBehaviour
+{
+
+    public bool _isIced = false;
+
+    public bool[] _casillaAdyacente = { true, true, true, true }; //Array de las casillas adyacentes: Arriba, derecha, abajo, izquierda. True = libre, False = ocupada
+
+    public float _width;
+    public float _heigth;
+
+
+        
+    public void setIced (bool iced) { _isIced = iced; }
+    public bool getIced(bool iced) { return _isIced; }
+
+    public void setAdyacent(bool[] adyacent) { _casillaAdyacente = adyacent; }
+    public bool[] getAdyacent() { return _casillaAdyacente; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector2( _width,_heigth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ El laberinto y mas/Assets/Scripts/MatrixCasillas.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixCasillas : MonoBehaviour
+{
+    public int numCasillasX;
+    public int numCasillasY;
+
+    public GameObject prefabCasilla;
+    public GameObject[,] casillas;
+
+    public float widthCasilla;
+    public float heigthCasilla;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        casillas = new GameObject[numCasillasX, numCasillasY];
+        for(int i = 0; i < numCasillasX; i++)
+        {
+            for(int j = 0; j < numCasillasY; j++)
+            {
+                casillas[i, j] = Instantiate(prefabCasilla);
+                casillas[i, j].transform.SetParent(transform);
+               
+                widthCasilla = prefabCasilla.GetComponent<Casilla>()._width;
+                heigthCasilla = prefabCasilla.GetComponent<Casilla>()._heigth;
+                casillas[i, j].transform.position = new Vector2(transform.position.x + (i * widthCasilla/1), transform.position.y - (j * heigthCasilla/1));
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
