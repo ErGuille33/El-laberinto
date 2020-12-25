18f16014ab9409a22f160ac2b63acec3ef24eadb
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2020-12-25 17:06:05 +0100

    Texto de nivel

--- El laberinto y mas/Assets/Scripts/GameManager.cs
+++ El laberinto y mas/Assets/Scripts/GameManager.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public LevelPackage[] levelPackages; 
     public LevelManager levelManager;
+
+    public Text textLevel;
 #if UNITY_EDITOR
     public int levelToPlay;
     public bool iceLevelsToPlay;
@@ -20,10 +23,16 @@ public class GameManager : MonoBehaviour
             DestroyImmediate(gameObject);
             return;
         }
-        if(iceLevelsToPlay)
+        if (iceLevelsToPlay)
+        {
             levelManager.setTextAsset(levelPackages[1].levels[levelToPlay]);
+            textLevel.text = "PISO DE HIELO" + " - " + (levelToPlay + 1);
+        }
         else
+        {
             levelManager.setTextAsset(levelPackages[0].levels[levelToPlay]);
+            textLevel.text = "CLASICO" + " - " + (levelToPlay + 1);
+        }
 
     }
 
