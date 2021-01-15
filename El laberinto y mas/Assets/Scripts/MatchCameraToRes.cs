a2290a096722ce8745ad1f8f1ddb2d12ddb93232
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2021-01-15 20:55:24 +0100

    Camara les goo

---
+++ El laberinto y mas/Assets/Scripts/MatchCameraToRes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchCameraToRes : MonoBehaviour
+{
+    public int Height;
+    public int Width;
+
+    public void Awake()
+    {
+        MatchCamera();
+    }
+
+    private void OnValidate()
+    {
+        MatchCamera();
+    }
+
+    private void MatchCamera()
+    {
+        var cam = GetComponent<Camera>();
+        if (cam == null) return;
+
+        var position = cam.ViewportToWorldPoint(Vector3.zero);
+        var up = cam.ViewportToWorldPoint(Vector3.up) - position;
+        var right = cam.ViewportToWorldPoint(Vector3.right) - position;
+
+        var matchSize = Mathf.Max(Height, Width * up.magnitude / right.magnitude);
+
+        cam.orthographicSize = matchSize;
+    }
+}
