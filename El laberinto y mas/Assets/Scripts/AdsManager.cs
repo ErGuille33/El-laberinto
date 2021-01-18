4991f2d15563e61466efbed4c24050063702eed3
GCidon <38065027+GCidon@users.noreply.github.com>
2021-01-18 15:57:27 +0100

    Lo de los anuncios

---
+++ El laberinto y mas/Assets/Scripts/AdsManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour
+{
+    private string gameId = "3974381";
+    private string placementId = "banner";
+    private bool testMode = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenInitialized());
+    }
+
+    IEnumerator ShowBannerWhenInitialized()
+    {
+        while(!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show(placementId);
+    }
+}
---
+++ El laberinto y mas/Assets/Scripts/RewardedAd.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class RewardedAd : MonoBehaviour, IUnityAdsListener
+{
+    string gameId = "3974381";
+    string myPlacementId = "rewardedVideo";
+    bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
+    }
+
+    public void ShowRewardedVideo()
+    {
+        if(Advertisement.IsReady(myPlacementId))
+        {
+            Advertisement.Show(myPlacementId);
+        }
+        else
+        {
+            Debug.Log("No esta ready hermano");
+        }
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if(showResult == ShowResult.Finished)
+        {
+            // Dar una Hint supongo
+        }
+        else if(showResult == ShowResult.Skipped)
+        {
+            // Te queda sin hint tonto
+        }
+        else if(showResult == ShowResult.Failed)
+        {
+            Debug.LogWarning("No ha terminado el ad por error");
+        }
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        if(placementId == myPlacementId)
+        {
+            // Para cuando ya puedes usar el boton de anuncio
+        }
+    }
+
+
+    public void OnUnityAdsDidError(string message)
+    {
+        // Log the error.
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        // Optional actions to take when the end-users triggers an ad.
+    }
+
+    public void OnDestroy()
+    {
+        Advertisement.RemoveListener(this);
+    }
+}
