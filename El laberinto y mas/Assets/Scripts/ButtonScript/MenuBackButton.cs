cca313c84b3d542382a4d7a949ed2bc7e9337501
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2021-01-21 20:33:21 +0100

    Ordenando y limpiando y home

---
+++ El laberinto y mas/Assets/Scripts/ButtonScript/MenuBackButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuBackButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameManager gm;
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(Back);
+    }
+
+    // Update is called once per frame
+    void Back()
+    {
+        if(GameManager._instance.state == GameManager.State.LEV)
+        {
+            GameManager._instance.state = GameManager.State.PACK;
+        }
+    }
+}
