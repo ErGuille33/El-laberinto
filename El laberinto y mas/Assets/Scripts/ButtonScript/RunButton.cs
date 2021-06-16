797dff5bded2ea7a302075927ee862f00582a6d6
GCidon <38065027+GCidon@users.noreply.github.com>
2021-06-16 23:33:36 +0200

    Botones

---
+++ El laberinto y mas/Assets/Scripts/ButtonScript/RunButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunButton : MonoBehaviour
+{
+    public void useHint()
+    {
+        GameManager._instance.Run();
+    }
+}
